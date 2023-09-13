58b65517847930963bc308d15559aa2ec60b7bcf
Carolina Silva <65463404+Carolina-Silva@users.noreply.github.com>
2023-09-13 10:59:49 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ConsultaCatalogo from "./components/ConsultaCatalogo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Bom dia</h1>
+      <ConsultaCatalogo />
+    </div>
+  );
+}
